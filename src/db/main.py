b57b00c447124950c92dafa9ecd0ace81c2ae0f7
Sungsoon Park <sungsoon.park@gmail.com>
2026-04-07 15:43:42 -0700

    Database Integration with SQLModel

---
+++ src/db/main.py
@@ -0,0 +1,26 @@
+from sqlmodel import create_engine, text
+from sqlalchemy.ext.asyncio import AsyncEngine
+from src.config import Config
+from sqlmodel import SQLModel
+from src.books.models import Book
+
+engine = AsyncEngine(create_engine(
+    url=Config.DATABASE_URL,
+    echo=True
+))
+
+# async def initdb():
+#     """create a connection to our db"""
+
+#     async with engine.begin() as conn:
+#         statement = text("select 'Hello World'")
+
+#         result = await conn.execute(statement)
+
+#         print(result)
+
+async def initdb():
+    """create our database models in the database"""
+
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
